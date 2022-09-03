86d10b9606eca3707f61eaacfeed1d74695bb541
jia0213 <984419317@qq.com>
2022-09-03 22:45:07 +0800

    Site updated: 2022-09-03 22:45:06

--- api/build.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-fs.writeFile('built-time.js', `module.exports = 'build-${new Date()}'`, (err) => {
-    if (err) throw err;
-    // console.log('Build time file created successfully!');
-});
