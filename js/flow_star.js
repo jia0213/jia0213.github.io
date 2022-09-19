7cb5cad7c9bfef1e921af9a9acbb61f4bbee85bb
jia0213 <984419317@qq.com>
2022-09-19 14:05:14 +0000

    若干新增 Signed-off-by: jia0213 <984419317@qq.com> Updated By Github Actions

--- js/flow_star.js
+++ js/flow_star.js
@@ -216,29 +216,15 @@ $(window).on('load', function () {
         });
     }
     window.scrollCollect && window.removeEventListener('scroll', window.scrollCollect || function(){});
-    // 51 统计
-    // $.ajax({
-    //     type:'post',
-    //     url:'https://v6-open.51.la/open/online/data',
-    //     'contentType':'application/json',
-    //     dataType:'json',
-    //     data:{
-    //         accessKey:'hOBiTMG5hmFlM9UPSHaXgSuAydGwfxTM',
-    //         nonce:'1225', //Math.random()*10+"".slice(0,4)
-    //         timestamp:'1662039695841',
-    //         sign:'D3E859A9120345C345F10E5038632601DCBFCF39CAB334AB030AF3629AFA8A96',
-    //         maskId:'JnlO5kK3V4jLxDHf',
-    //         type:'TERMINAL',
-    //         minute:'30'
-    //     }
-    // })
 
     // const _apiBaseUrl = "http://localhost:3000";
     const _apiBaseUrl = "";
     // 记录访问信息
     let isRecording = false;
-    
-    if (location.pathname=="/") getPv();;
+    if(!sessionStorage.login){
+        sessionStorage.login = new Date();
+        getPv();
+    }
     function setPvAjax(data){
         if(isRecording)return;
         isRecording = true;
@@ -312,5 +298,75 @@ $(window).on('load', function () {
         let str = `<div class="dv-border-box-1-wrap"><div class="dv-border-box-1"> <svg width="150px" height="150px" class="left-top border"><polygon points="6,66 6,18 12,12 18,12 24,6 27,6 30,9 36,9 39,6 84,6 81,9 75,9 73.2,7 40.8,7 37.8,10.2 24,10.2 12,21 12,24 9,27 9,51 7.8,54 7.8,63" fill="#4fd2dd"><animate attributeName="fill" values="#4fd2dd;#235fa7;#4fd2dd" dur="0.5s" begin="0s" repeatCount="indefinite"></animate></polygon> <polygon points="27.599999999999998,4.8 38.4,4.8 35.4,7.8 30.599999999999998,7.8" fill="#235fa7"><animate attributeName="fill" values="#235fa7;#4fd2dd;#235fa7" dur="0.5s" begin="0s" repeatCount="indefinite"></animate></polygon> <polygon points="9,54 9,63 7.199999999999999,66 7.199999999999999,75 7.8,78 7.8,110 8.4,110 8.4,66 9.6,66 9.6,54" fill="#4fd2dd"><animate attributeName="fill" values="#4fd2dd;#235fa7;transparent" dur="1s" begin="0s" repeatCount="indefinite"></animate></polygon></svg><svg width="150px" height="150px" class="right-top border"><polygon points="6,66 6,18 12,12 18,12 24,6 27,6 30,9 36,9 39,6 84,6 81,9 75,9 73.2,7 40.8,7 37.8,10.2 24,10.2 12,21 12,24 9,27 9,51 7.8,54 7.8,63" fill="#4fd2dd"><animate attributeName="fill" values="#4fd2dd;#235fa7;#4fd2dd" dur="0.5s" begin="0s" repeatCount="indefinite"></animate></polygon> <polygon points="27.599999999999998,4.8 38.4,4.8 35.4,7.8 30.599999999999998,7.8" fill="#235fa7"><animate attributeName="fill" values="#235fa7;#4fd2dd;#235fa7" dur="0.5s" begin="0s" repeatCount="indefinite"></animate></polygon> <polygon points="9,54 9,63 7.199999999999999,66 7.199999999999999,75 7.8,78 7.8,110 8.4,110 8.4,66 9.6,66 9.6,54" fill="#4fd2dd"><animate attributeName="fill" values="#4fd2dd;#235fa7;transparent" dur="1s" begin="0s" repeatCount="indefinite"></animate></polygon></svg><svg width="150px" height="150px" class="left-bottom border"><polygon points="6,66 6,18 12,12 18,12 24,6 27,6 30,9 36,9 39,6 84,6 81,9 75,9 73.2,7 40.8,7 37.8,10.2 24,10.2 12,21 12,24 9,27 9,51 7.8,54 7.8,63" fill="#4fd2dd"><animate attributeName="fill" values="#4fd2dd;#235fa7;#4fd2dd" dur="0.5s" begin="0s" repeatCount="indefinite"></animate></polygon> <polygon points="27.599999999999998,4.8 38.4,4.8 35.4,7.8 30.599999999999998,7.8" fill="#235fa7"><animate attributeName="fill" values="#235fa7;#4fd2dd;#235fa7" dur="0.5s" begin="0s" repeatCount="indefinite"></animate></polygon> <polygon points="9,54 9,63 7.199999999999999,66 7.199999999999999,75 7.8,78 7.8,110 8.4,110 8.4,66 9.6,66 9.6,54" fill="#4fd2dd"><animate attributeName="fill" values="#4fd2dd;#235fa7;transparent" dur="1s" begin="0s" repeatCount="indefinite"></animate></polygon></svg><svg width="150px" height="150px" class="right-bottom border"><polygon points="6,66 6,18 12,12 18,12 24,6 27,6 30,9 36,9 39,6 84,6 81,9 75,9 73.2,7 40.8,7 37.8,10.2 24,10.2 12,21 12,24 9,27 9,51 7.8,54 7.8,63" fill="#4fd2dd"><animate attributeName="fill" values="#4fd2dd;#235fa7;#4fd2dd" dur="0.5s" begin="0s" repeatCount="indefinite"></animate></polygon> <polygon points="27.599999999999998,4.8 38.4,4.8 35.4,7.8 30.599999999999998,7.8" fill="#235fa7"><animate attributeName="fill" values="#235fa7;#4fd2dd;#235fa7" dur="0.5s" begin="0s" repeatCount="indefinite"></animate></polygon> <polygon points="9,54 9,63 7.199999999999999,66 7.199999999999999,75 7.8,78 7.8,110 8.4,110 8.4,66 9.6,66 9.6,54" fill="#4fd2dd"><animate attributeName="fill" values="#4fd2dd;#235fa7;transparent" dur="1s" begin="0s" repeatCount="indefinite"></animate></polygon></svg> </div></div>`;
         $("#aside-content .card-info").append($(str));
     }
-    
+    let o = {
+        setDom(data){
+            let list = data || [{'title':'说点什么','content':'动态说说',time:new Date()},{'title':'说点什么','content':'动态说说2',time:new Date()},{'title':'说点什么','content':'动态说说3',time:new Date()}] ;
+            let str = '';
+            // str += '<div class="swiper mySwiper">';
+            str += '<div class="swiper-wrapper">';
+            list.map(v=>{
+                // ${v.title?('<p class="title-row">'+v.title+'</p>'):''}
+                // <p class="content-row">${v.content}</p>
+                // <div class="time-zone">${v.time}</div>
+                str += `
+                    <div class="swiper-slide" style="background:${random_color()};">${v.content}</div>
+                `;
+            });
+            // str+='</div>';
+            str+='</div>';
+            $(str).prependTo($('.mySwiper'));
+            if(Swiper){
+                var swiper = new Swiper(".mySwiper", {
+                    // effect: "cards",
+                    effect: "coverflow",
+                    // parallax: true,
+                    grabCursor: true,
+                    loop:true,
+                    autoplay:true,
+                    grabCursor: true,
+                    cubeEffect: {
+                    shadow: true,
+                    slideShadows: true,
+                    shadowOffset: 20,
+                    shadowScale: 0.94,
+                    }
+                });
+            }
+        },
+        getList() {
+            this.setDom();
+            let that = this;
+            $.ajax({
+                type: 'post',
+                url: '/api/myTalk',
+                success({ result, data }) {
+                    if (result) {
+                        that.setDom([...data]);
+                    }
+                }
+            })
+        },
+    }
+    o.getList();
 })
+function random_color(){
+    return '#' + parseInt(Math.random()*0xffffff).toString(16)
+}
+function setCssLink(urls) {
+if (!urls) return;
+if ('string' === typeof urls) insertLink(urls);
+if (Array.isArray(urls)) urls.forEach(item => insertLink(item));
+function insertLink(link) {
+var linkDom;
+// css 内联样式
+if (/\S\{(.)*\}*/.test(link)) {
+linkDom = document.createElement('style');
+linkDom.innerHTML = link;
+} else {
+linkDom = document.createElement('link');
+linkDom.setAttribute('rel', 'stylesheet');
+linkDom.setAttribute('href', link);
+}
+document.head.appendChild(linkDom);
+}
+}
